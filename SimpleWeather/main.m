47e1cfaeb018d1781aca12c7d21bd0fe5ad39c8a
Yukui Ye <mollysmile.ye@gmail.com>
2014-01-06 13:56:16 -0500

    Initial Commit

---
+++ SimpleWeather/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleWeather
+//
+//  Created by Yukui ye on 1/6/14.
+//  Copyright (c) 2014 M.Shanken. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SimpleWeatherAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SimpleWeatherAppDelegate class]));
+    }
+}
